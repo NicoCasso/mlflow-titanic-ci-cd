1daaaca06ac2b00ba3f2f70743f0e8c0c0fba1c4
nicolas <nicolas.cassonnet@wanadoo.fr>
2025-05-12 15:08:40 +0200

    same correction that on windows when saving pickle

--- src/utils.py
+++ src/utils.py
@@ -7,13 +7,21 @@ import pickle
 #______________________________________________________________________________
 def print_divider(title):
     print('\n{} {} {}\n'.format('-' * 25, title, '-' * 25))
+    
 #______________________________________________________________________________
 #
 # region load_pickle
 #______________________________________________________________________________
 def load_pickle(file_path):
-    if os.path.getsize(file_path) > 0:  # Vérifie que le fichier n'est pas vide
-        with open(file_path, 'rb') as f:
-            return pickle.load(f)
-    else:
-        raise ValueError(f"Le fichier {file_path} est vide.")
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"Le fichier {file_path} n'existe pas.")
+    
+    if os.path.getsize(file_path) == 0:  # Vérifie que le fichier n'est pas vide
+        raise EOFError(f"Le fichier {file_path} est vide.")
+    
+    return_value = None
+    with open(file_path, 'rb') as f:
+        return_value = pickle.load(f)
+
+    return return_value
+        
