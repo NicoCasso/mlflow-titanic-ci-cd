40cae59c836cfff767e5221e06ad124efebbe1b9
nicolas <nicolas.cassonnet@wanadoo.fr>
2025-05-12 16:09:08 +0200

    first check

---
+++ tests/test_basic.py
@@ -0,0 +1,12 @@
+def test_one_plus_one_equals_two() :
+    assert 1 + 1 == 2 
+
+def test_one_plus_one_equals_three() :
+    assert 1 + 1 == 3
+
+def test_one_plus_two_equals_two() :
+    assert 1 + 2 == 2
+
+def test_one_plus_two_equals_three() :
+    assert 1 + 2 == 3
+
